de2a4115b3cd7956202e655d3342b4a2723d4720
github username <github email address>
2019-11-11 12:43:33 -0600

    chat messages not syncing

--- src/containers/messagesContainer.js
+++ src/containers/messagesContainer.js
@@ -18,8 +18,8 @@ class MessagesContainer extends Component {
         }
     }
 
-    renderChatMessages = () => {
-        return this.props.project.chat_room.messages.map( (message, key) => {
+    renderChatMessages = (messages) => {
+        return messages.map( (message, key) => {
             return <li key={key} className={message.user_id === this.props.user.id ? 'chat-message w-50 shadow text-right blue lighten-2 rounded-pill ml-auto' : 'chat-message w-50 shadow mr-auto text-left purple lighten-3 rounded-pill'}>{message.text}</li>
         });
     };
@@ -29,7 +29,7 @@ class MessagesContainer extends Component {
         <div id='incoming-messages' className='shadow p-3 mb-3 bg-white rounded overflow-auto'>
             <MDBContainer className='pt-1 pl-1 pr-1'>
                 <MDBListGroup className='w-100'>
-                    {this.renderChatMessages()}
+                    {this.renderChatMessages(this.props.project.chat_room.messages)}
                 </MDBListGroup>
             </MDBContainer>
         </div> );
