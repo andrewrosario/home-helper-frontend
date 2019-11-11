95a72df5fb0b0032768ccda238037d5591c99a5d
github username <github email address>
2019-11-11 10:50:24 -0600

    added automatic enter chat room on create project

--- src/actions/createNewProject.js
+++ src/actions/createNewProject.js
@@ -1,3 +1,6 @@
+import { leaveChatRoom } from '../functions/leaveChatRoom'
+import { enterChatRoom } from '../functions/enterChatRoom'
+
 export function createNewProject(project) {
     const formData = new FormData()
     const { user, name, description, type, before_photos } = project
@@ -10,7 +13,11 @@ export function createNewProject(project) {
         formData.append('before_photos[]', file, file.name);
     }
 
-    return (dispatch) => {
+    return (dispatch, getState) => {
+        const { ProjectReducer, UserReducer } = getState()
+        const novice_projects = UserReducer.currentUser.novice_projects
+        console.log('createNewProject getState', ProjectReducer, UserReducer )
+        leaveChatRoom(UserReducer.socket, ProjectReducer.currentProject.chat_room)
         dispatch({type: 'START_CREATE_PROJECT_REQUEST'}, project)
         fetch(`${process.env.REACT_APP_API_URL}/projects`, {
             method: 'POST',
@@ -28,6 +35,7 @@ export function createNewProject(project) {
         })
         .then(user => {
             dispatch({ type: "FINISH_CREATE_PROJECT", user})
+            enterChatRoom(UserReducer.socket, UserReducer.currentUser.novice_projects[novice_projects.length - 1].id)
         })
         .catch(error => console.log(error))
     }
