e13fa9483c326f9f015833dd03667056161b7b64
github username <github email address>
2019-11-12 14:03:06 -0600

    persist reducer

--- src/localStorage.js
+++ src/localStorage.js
@@ -4,15 +4,16 @@ export const saveState = (state) => {
       const serializedState = JSON.stringify(state);
       console.log('serialized state', serializedState)
       localStorage.setItem('state', serializedState);
-    } catch {
+    } catch(error) {
       // ignore write errors
-      console.log('saveState errors')
+      console.log('saveState errors', error)
     }
   };
 
   export const loadState = () => {
     try {
       const serializedState = localStorage.getItem('state');
+      console.log(localStorage.getItem('state'))
       if (serializedState === null) {
         return undefined;
       }
