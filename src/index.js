a1e6b1801c74313130527c551bd17065d34f99c3
github username <github email address>
2019-11-12 12:54:50 -0600

    persist reducer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import rootReducer from './reducers';
 import { Provider } from 'react-redux'
 import rootReducer from './reducers/index'
 import thunk from 'redux-thunk';
@@ -10,6 +15,14 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import './index.scss';
 import App from './App';
 import { loadState, saveState } from './localStorage'
+
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  stateReconciler: autoMergeLevel2 // see "Merge Process" section for details.
+ };
+ 
+const pReducer = persistReducer(persistConfig, rootReducer);
 
 const persistedState = loadState();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -29,7 +42,9 @@ store.subscribe(() => {
 // Higher order component design pattern
 ReactDOM.render(
     <Provider store={store}>
+      <PersistGate loading={<LoadingView />} persistor={persistor}>
         <App />
+      </PersistGate>
     </Provider>
     , document.getElementById('root')
 );
