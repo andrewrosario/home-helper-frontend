a1965b9e66fa71764ceae9acf6bbe917dd86cf58
github username <github email address>
2019-11-12 10:23:07 -0600

    add root path to express use

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express')();
 const port = process.env.PORT || 8000;
 const server = express()
   .use('/', express.static(__dirname + '/../../build'))
