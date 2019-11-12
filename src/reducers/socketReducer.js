1b1fc49fb421a1a0a1d4868112ab65c68893023f
github username <github email address>
2019-11-12 15:31:15 -0600

    debug change in reducers

--- src/reducers/socketReducer.js
+++ src/reducers/socketReducer.js
@@ -1,6 +1,7 @@
 export default function SocketReducer( state = { socket: null }, action) {
     switch (action.type) {
         case "OPEN_SOCKET":
+            console.log('open socket', action)
             return {
                 socket: action.socket,
             }
