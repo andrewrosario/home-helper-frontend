d7fe065d7682f1d879a10f333b146b4a9f878971
github username <github email address>
2019-11-12 10:19:36 -0600

    add root path to express use

--- src/server/index.js
+++ src/server/index.js
@@ -3,13 +3,14 @@ const port = process.env.PORT || 8000;
 const server = express()
   .use('/', express.static(__dirname + '/../../build'))
   .listen(port, () => console.log(`Listening on ${ port }`))
-  .get('*', function(req, res) {
+
+server.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../../public/index.html'), function(err) {
-      if (err) {
+        if (err) {
         res.status(500).send(err)
-      }
+        }
     })
-  })
+})
 var io = require('socket.io')(server);
 // var app = express()
 
