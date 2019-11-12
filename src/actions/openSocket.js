225b96b7ee3bf4951b2bb0e9e2587f948eea4d1f
github username <github email address>
2019-11-12 16:24:02 -0600

    debug change in reducers

--- src/actions/openSocket.js
+++ src/actions/openSocket.js
@@ -1,6 +1,6 @@
 export function openSocket(socket) {
     console.log('action open socket', socket)
-    return(dispatch) => {
-        dispatch({type: 'OPEN_SOCKET', socket})
+    return (dispatch) => {
+        dispatch({type: 'OPEN_SOCKET'})
     }
 }
